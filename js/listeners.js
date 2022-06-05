ca8616de8c1d38d839804f97ed0863b2e26790bd
sara <sara@wondercraft.co>
2022-06-04 23:17:21 -0500

    test 2

--- js/listeners.js
+++ js/listeners.js
@@ -1,86 +1,92 @@
 const thankYou = (e) => {
-    e.target.parentElement.classList.add('hide');
-        const par = e.target.closest('.card__form');
-        par.querySelector('.card__voteagain').classList.remove('hide');
-        par.querySelector('.card__form__quickinfo .thank').classList.remove('hide');
-        par.querySelector('.card__form__quickinfo .info').classList.add('hide');
-        e.target.disabled = true;
-        e.target.parentElement.querySelectorAll('.card__vote').forEach(ele => ele.classList.remove('active'));
- 
-
-}
+  e.target.parentElement.classList.add("hide");
+  const par = e.target.closest(".card__form");
+  par.querySelector(".card__voteagain").classList.remove("hide");
+  par.querySelector(".card__form__quickinfo .thank").classList.remove("hide");
+  par.querySelector(".card__form__quickinfo .info").classList.add("hide");
+  e.target.disabled = true;
+  e.target.parentElement
+    .querySelectorAll(".card__vote")
+    .forEach((ele) => ele.classList.remove("active"));
+};
 const voteAgain = (e) => {
-    const box= e.target.parentElement;
-    box.querySelector('.card__votebox').classList.remove('hide');
-    box.querySelector('.card__form__quickinfo .thank').classList.add('hide');
-    box.querySelector('.card__form__quickinfo .info').classList.remove('hide');
-    e.target.classList.add('hide');
-}
-const sumVote = (index, jsonData, finalVote) => {   
-    let persona = jsonData.data[index];
-    
-    
-    if (finalVote == 'yes'){
-        persona.votes.positive++; 
-    } else if (finalVote == 'no'){
-        persona.votes.negative++; 
+  const box = e.target.parentElement;
+  box.querySelector(".card__votebox").classList.remove("hide");
+  box.querySelector(".card__form__quickinfo .thank").classList.add("hide");
+  box.querySelector(".card__form__quickinfo .info").classList.remove("hide");
+  e.target.classList.add("hide");
+};
+const sumVote = (index, jsonData, finalVote) => {
+  let persona = jsonData.data[index];
+
+  if (finalVote == "yes") {
+    persona.votes.positive++;
+  } else if (finalVote == "no") {
+    persona.votes.negative++;
+  }
+  let totalvotes = persona.votes.positive + persona.votes.negative;
+  let posPercent = ((persona.votes.positive * 100) / totalvotes).toFixed(2);
+  let negPercent = ((persona.votes.negative * 100) / totalvotes).toFixed(2);
+
+  document.querySelector(
+    `#results-${index} .bar .positive`
+  ).style.width = `${posPercent}%`;
+  document.querySelector(
+    `#results-${index} .bar .negative`
+  ).style.width = `${negPercent}%`;
+  document.querySelector(
+    `#results-${index} .data .label .pos`
+  ).textContent = `${posPercent}%`;
+  document.querySelector(
+    `#results-${index} .data .label .neg`
+  ).textContent = `${negPercent}%`;
+
+  localStorage.setItem("jsonData", JSON.stringify(jsonData));
+};
+
+function listeners(jsonData) {
+  let valselect = document.querySelector("#styleview");
+  valselect.addEventListener("change", () => {
+    if (valselect.value === "grid") {
+      document.querySelector(".peoplecard").classList.remove("list");
+      document.querySelector(".peoplecard").classList.add("grid");
+    } else if (valselect.value === "list") {
+      document.querySelector(".peoplecard").classList.remove("grid");
+      document.querySelector(".peoplecard").classList.add("list");
     }
-    let totalvotes = persona.votes.positive + persona.votes.negative;
-    let posPercent = (persona.votes.positive * 100 / totalvotes).toFixed(2);
-    let negPercent = (persona.votes.negative * 100 / totalvotes).toFixed(2);
-   
-    document.querySelector(`#results-${index} .bar .positive`).style.width =  `${posPercent}%`;
-    document.querySelector(`#results-${index} .bar .negative`).style.width= `${negPercent}%`;
-    document.querySelector(`#results-${index} .data .label .pos`).textContent =  `${posPercent}%`;
-    document.querySelector(`#results-${index} .data .label .neg`).textContent= `${negPercent}%`;
+  });
 
-    localStorage.setItem('jsonData', JSON.stringify(jsonData));
-        console.log(jsonData.data[index]);
-}
-
-function listeners (jsonData) {
-    let valselect = document.querySelector('#styleview'); 
-        valselect.addEventListener('change', () => {
-        if( valselect.value === 'grid'){
-            document.querySelector('.peoplecard').classList.remove('list');
-            document.querySelector('.peoplecard').classList.add('grid');
-        } else if ( valselect.value === 'list'){
-            document.querySelector('.peoplecard').classList.remove('grid');
-            document.querySelector('.peoplecard').classList.add('list');
-        }
-    }); 
-
-    document.querySelectorAll('.card__votebox').forEach( vote =>  {
-        let finalVote = 'null' ;
-        vote.addEventListener('click', (e) => {
-        if( e.target.className.indexOf('card__vote yes') != -1) {
-            e.target.classList.add('active');
-            const voteBox = e.target.parentElement;
-            voteBox.children[1].classList.remove('active');
-            finalVote = 'yes';
-            voteBox.children[2].classList.remove('disabled');
-            voteBox.children[2].disabled = false;
-        }
-        if( e.target.className.indexOf('card__vote no') != -1) {
-            e.target.classList.add('active');
-            e.target.classList.add('active');
-            const voteBox = e.target.parentElement;
-            voteBox.children[0].classList.remove('active');
-            finalVote = 'no';
-            voteBox.children[2].classList.remove('disabled');
-            voteBox.children[2].disabled = false;
-        }
-        if( e.target.className.indexOf('card__votesubmit') != -1) {
-            const ind = e.target.parentElement.id;
-            sumVote(ind,jsonData, finalVote);        
-            thankYou(e)
-            }
-        })
+  document.querySelectorAll(".card__votebox").forEach((vote) => {
+    let finalVote = "null";
+    vote.addEventListener("click", (e) => {
+      if (e.target.className.indexOf("card__vote yes") != -1) {
+        e.target.classList.add("active");
+        const voteBox = e.target.parentElement;
+        voteBox.children[1].classList.remove("active");
+        finalVote = "yes";
+        voteBox.children[2].classList.remove("disabled");
+        voteBox.children[2].disabled = false;
+      }
+      if (e.target.className.indexOf("card__vote no") != -1) {
+        e.target.classList.add("active");
+        e.target.classList.add("active");
+        const voteBox = e.target.parentElement;
+        voteBox.children[0].classList.remove("active");
+        finalVote = "no";
+        voteBox.children[2].classList.remove("disabled");
+        voteBox.children[2].disabled = false;
+      }
+      if (e.target.className.indexOf("card__votesubmit") != -1) {
+        const ind = e.target.parentElement.id;
+        sumVote(ind, jsonData, finalVote);
+        thankYou(e);
+      }
     });
+  });
 
-    document.querySelectorAll('.card__voteagain').forEach( again =>{
-        again.addEventListener('click', (e) =>{
-            voteAgain(e);
-        })
+  document.querySelectorAll(".card__voteagain").forEach((again) => {
+    again.addEventListener("click", (e) => {
+      voteAgain(e);
     });
+  });
 }
--- js/main.js
+++ js/main.js
@@ -1,64 +1,67 @@
-  const getData = async () => {
-      
-    if(!window.localStorage.getItem('jsonData')){
-        const response = await fetch("../assets/data.json");
-        const data = await response.json();
-        return data;}
-    else {
-        const json = window.localStorage.getItem('jsonData');
-        console.log(json, 'json')
-        const data = JSON.parse(json)
-        console.log(data.data[1].votes, 'hd')
-        return data;
-    }
+const getData = async () => {
+  if (!window.localStorage.getItem("jsonData")) {
+    const response = await fetch("/assets/data.json");
+    const data = await response.json();
+    return data;
+  } else {
+    const json = window.localStorage.getItem("jsonData");
+    const data = JSON.parse(json);
+    return data;
+  }
+};
+
+//console.log(data);
+function setTime(dat) {
+  const datelast = dat.split("T");
+  const datea = datelast[0];
+  const dateago = datea.split("-");
+  const today = new Date();
+  const lastDay = new Date(dateago);
+  let timedif = today.getTime() - lastDay.getTime();
+  let daydiff = timedif / (1000 * 3600 * 24);
+
+  let time = "";
+  if (daydiff / 365 > 1) {
+    let year = Math.round(daydiff / 365);
+    time = year = 1 ? year + " year ago" : year + " years ago";
+  } else if (daydiff / 30 > 1) {
+    let months = Math.round(daydiff / 30);
+    time = months = 1 ? months + " month ago" : months + " months ago";
+  } else if (daydiff / 30 < 1) {
+    let days = Math.round(daydiff);
+    time = days = 1 ? days + " day ago" : days + " days ago";
+  }
+  return time;
 }
 
-//console.log(data); 
-function setTime (dat){
-    const datelast = dat.split('T');
-    const datea = datelast[0];
-    const dateago = datea.split('-');
-    const today = new Date();
-    const lastDay = new Date(dateago);
-    let timedif = today.getTime() -lastDay.getTime();
-    let daydiff = timedif  / (1000 * 3600 * 24);
-    
-    let time = '';
-        if (daydiff / 365 > 1){
-            let year = Math.round(daydiff / 365)
-            time =  year = 1 ? year + ' year ago' : year + ' years ago'
-        } else if (daydiff / 30 > 1 ){
-            let months = Math.round(daydiff / 30)
-            time = months = 1 ? months + ' month ago' : months + ' months ago';
-        }else if ( daydiff / 30 < 1 ){
-            let days = Math.round(daydiff);
-            time = days = 1 ? days +' day ago' : days + ' days ago'
-        }
- return time; 
-}
+function builtCard(person, index) {
+  let totalvotes = person.votes.positive + person.votes.negative;
 
-function builtCard (person, index) {
-    let totalvotes = person.votes.positive + person.votes.negative;
-    
-    let posPercentage = (person.votes.positive * 100 / totalvotes).toFixed(2);
-    let negPercentage = (person.votes.negative * 100 / totalvotes).toFixed(2);
-    const finalres = posPercentage > negPercentage ? 'thumbsup' : 'thumbsdown';
-    const cardHTML = `<div class="card">
-<div class="card__picture" style="background-image: url('../assets/img/people/${person.picture}'");>
+  let posPercentage = ((person.votes.positive * 100) / totalvotes).toFixed(2);
+  let negPercentage = ((person.votes.negative * 100) / totalvotes).toFixed(2);
+  const finalres = posPercentage > negPercentage ? "thumbsup" : "thumbsdown";
+  const cardHTML = `<div class="card">
+<div class="card__picture" style="background-image: url('../assets/img/people/${
+    person.picture
+  }'");>
 
 </div>
 
 <div class="card__content">
     
     <div class="card__info">
-        <div class="card__info__name ${finalres}"><span>${person.name}</span></div>
+        <div class="card__info__name ${finalres}"><span>${
+    person.name
+  }</span></div>
          <p class="card__info__description">${person.description}</p>
     </div>
     
 
     <div class="card__form">
         <p class="card__form__quickinfo">
-            <span class="info">${setTime(person.lastUpdated)} in ${person.category}</span>
+            <span class="info">${setTime(person.lastUpdated)} in ${
+    person.category
+  }</span>
             <span class="thank hide">Thank you for your vote!</span>
         </p>
         <div id="${index}" class="card__votebox">
@@ -82,27 +85,26 @@ function builtCard (person, index) {
 </div>
 </div>
 `;
-    return cardHTML;
-
+  return cardHTML;
 }
 
-
 const aCards = (datos) => {
-    //console.log({datos})
-    const cards = `
-        ${datos.map((person, index)=>{
+  //console.log({datos})
+  const cards = `
+        ${datos
+          .map((person, index) => {
             const niw = builtCard(person, index);
             return niw;
-        }).join('')}`;
-    return cards;
-    
+          })
+          .join("")}`;
+  return cards;
 };
 
-(async function (){
-    const jsonData = await getData();
-    console.log(jsonData.data[1].votes);
-    const finalHMTL = aCards(jsonData.data);
-    document.querySelector('.peoplecard').innerHTML = finalHMTL;
-    listeners(jsonData);
-    localStorage.setItem('jsonData', JSON.stringify(jsonData));
-  })()
+(async function () {
+  const jsonData = await getData();
+  console.log(jsonData.data[1].votes);
+  const finalHMTL = aCards(jsonData.data);
+  document.querySelector(".peoplecard").innerHTML = finalHMTL;
+  listeners(jsonData);
+  localStorage.setItem("jsonData", JSON.stringify(jsonData));
+})();
